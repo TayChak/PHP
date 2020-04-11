0bbe7e5b679ded74fec68f2a62095aeee077b673
TheElite <47625834+TayChak@users.noreply.github.com>
2020-04-11 03:14:18 +0200

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<?php
+$var1 = "Hello world!";
+$x = 15;
+$y = 20.5;
+
+echo $var1;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+echo "<hr />";
+
+$sport= "Football";
+echo "I like $sport!";
+echo "<hr />";
+
+$sport= "Football";
+echo "I like " . $sport . "!";
+echo "<hr />";
+
+$x = 5;
+$y = 6;
+echo $x + $y;
+
+?>
+
+</body>
+</html>
+
+/*  
+
+There are some basic rules when naming variables a variable must start with the dollar sign followed
+
+by the name of the variable 
+A variable name must start with the letter or the underscore character 
+A variable name cannot start with a number.
+A variable name can only contain alphanumeric characters and underscores variable names are case sensitive.
+
+When working with variables the echo statement is often used to output the data. */
