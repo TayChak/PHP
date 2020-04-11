15a1fd18c25f430e9ae1ff46bff5e0df635d1058
TheElite <47625834+TayChak@users.noreply.github.com>
2020-04-11 03:28:31 +0200

    Create send.php

---
+++ send.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+
+First Name: <?php echo $_GET["fname"]; ?><br>  <!-- $_POST["fname"]; -->
+Email: <?php echo $_GET["email"]; ?>  <!-- $_POST["email"]; -->
+
+</body>
+</html>
