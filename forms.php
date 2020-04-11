68f6bc07d9e8f90676ebd8717d522b4e0aebd572
TheElite <47625834+TayChak@users.noreply.github.com>
2020-04-11 03:26:52 +0200

    Create forms.php

---
+++ forms.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+
+<form action="send.php" method="GET"> <!-- We used the GET methode send.php?fname=&email=
+	First Name: <input type="text" name="fname"><br>
+	E-mail: <input type="text" name="email"><br>
+	<input type="submit">
+</form>
+
+</body>
+</html>
